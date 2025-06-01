31d7ec5b115e357ef647e2fcf5a7db9f7db09040
ANDI MUHAMMAD IHSANUL HAQ MALVIRA <ihsannul370@gmail.com>
2025-06-01 19:23:41 +0700

    Update fitur buatan ihsan : fitur reviews controller, migration, seeder

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
+// Basic Routes
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+
+// Dashboard Route
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
